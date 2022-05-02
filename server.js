f817411ee6670fcdc0ffdbe719fcf7b68ca00226
Benjamin Venezia <benjamin.venezia@crea-inseec.com>
2022-05-02 18:50:13 +0200

    begin adding backend and basic context

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const app = express()
+
+const port = process.env.PORT || 5004
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
