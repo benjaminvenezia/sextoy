1eca6905404699dab32103c2676586af5753a9af
Benjamin Venezia <benjamin.venezia@crea-inseec.com>
2022-05-06 20:16:38 +0200

    sextoy query is alright in postman

--- controllers/sextoyController.js
+++ controllers/sextoyController.js
@@ -4,10 +4,12 @@ import mongoose from 'mongoose'
 
 const getSextoyByTags = async (req, res) => {
   const { tags } = req.query
-  console.log('req query: ', tags)
+  console.log('req query: ', typeof tags)
+
+  const tagsArray = tags.split(', ')
 
   const sextoys = await Sextoy.find({
-    tags_match: { $all: tags },
+    tags_match: { $all: tagsArray },
   })
 
   res.status(StatusCodes.OK).json({ sextoys, count: sextoys.length })
