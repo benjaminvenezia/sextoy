4f7f9b4425a8c53bc509ac14ec57e4092c58d053
Benjamin Venezia <benjamin.venezia@crea-inseec.com>
2022-05-10 21:21:10 +0200

    improve buttons sextoy category

--- client/src/components/SextoyArticle.js
+++ client/src/components/SextoyArticle.js
@@ -1,24 +1,32 @@
 import styled from 'styled-components'
+import { HipsterButton } from './Buttons'
 
 const SextoyArticle = ({ article }) => {
   return (
     <Wrapper>
       <h3>{article.name_shop}</h3>
-      <a className="btn" target="_blank" href={article.url_category}>
-        Visiter la catégorie!
-      </a>
-      <a className="btn" target="_blank" href={article.url_product}>
-        {article.name_shop} vous recommande ce produit!
-      </a>
+      <div className="buttons-container">
+        <HipsterButton as="a" href={article.url_category}>
+          Visiter la catégorie
+        </HipsterButton>
+        <HipsterButton as="a" href={article.url_product}>
+          {article.name_shop} vous recommande ce produit!
+        </HipsterButton>
+      </div>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
+  box-shadow: 0px 0px 5px white;
   background-color: var(--black);
   padding: 1rem;
   border-radius: 10px;
   margin-bottom: 10px;
+
+  .buttons-container {
+    display: flex;
+  }
 `
 
 export default SextoyArticle
