c32b5091c24cae4955f15cba530562a0dd94a46d
Benjamin Venezia <benjamin.venezia@crea-inseec.com>
2022-05-10 20:31:06 +0200

    increase nomatch response adding a button to return home

--- client/src/components/SextoyCategory.js
+++ client/src/components/SextoyCategory.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { SexToyCategoryCard } from '.'
+import { SexToyCategoryCard, HipsterButton } from '.'
+import { Link } from 'react-router-dom'
 
 const SextoyCategory = ({ tagsNextQuestion }) => {
   let url2 = `/api/v1/sextoy/tags?tags=${tagsNextQuestion.join(', ')}`
@@ -26,13 +27,28 @@ const SextoyCategory = ({ tagsNextQuestion }) => {
           return <SexToyCategoryCard key={category._id} category={category} />
         }))
       ) : (
-        <p>Malheureusement, aucune catégorie n'a matché avec vos conditions! 🥺</p>
+        <>
+          <p>
+            Malheureusement, aucune catégorie n'a matché avec vos conditions! 🥺{' '}
+            <br />
+            Nous oeuvrons actuellement pour augmenter le champs des possibles! Merci
+            de réessayer prochainement!
+            <HipsterButton as="a" href="/">
+              Revenir à la homepage
+            </HipsterButton>
+          </p>
+        </>
       )}
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  p {
+    color: var(--primary);
+  }
   padding: 2rem;
 `
 export default SextoyCategory
