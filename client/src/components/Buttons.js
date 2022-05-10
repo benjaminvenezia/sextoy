b00a2c923a9cfc80ecce18766c602884a257ccd5
Benjamin Venezia <benjamin.venezia@crea-inseec.com>
2022-05-10 22:02:40 +0200

    update buttons

--- client/src/components/Buttons.js
+++ client/src/components/Buttons.js
@@ -1,19 +1,21 @@
 import styled, { css } from 'styled-components'
 
 export const DefaultButton = styled.button`
-  background: #645cff;
-  border-radius: 25px;
+  border-radius: 40px;
   cursor: pointer;
-  width: 150px;
-  height: 50px;
+  width: 200px;
+  font-size: 0.7rem;
+  height: 70px;
   border: none;
   text-transform: capitalize;
   display: block;
-
   margin: 1rem auto;
 `
 
 export const HipsterButton = styled(DefaultButton)`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
   background: transparent;
   color: ${(props) => (props.disabled ? 'gray' : '#F6EBD8')};
   border: ${(props) => (props.disabled ? '1px solid gray' : '1px solid #F6EBD8')};
