30e201f95c63f3d2e0e1c12af5bf3139dcd33828
unknown <demiriardit9>
2022-05-10 11:51:57 +0200

    navbar implementation

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,25 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
+
 const Navbar = () => {
   return (
     <Wrapper>
       <div className="nav-center">
-        <Link to="/">Homepage</Link>
-        <Link to="login">Login</Link>
+        <img className="LogoNav" src="LogoDesyre.svg" alt="" />
+
+        <div className='containerNavElmt' >
+          <Link to="/">Desyre</Link>
+          <Link to="/">Homme</Link>
+          <Link to="/">Femme</Link>
+          <Link to="/">Couple</Link>
+          <Link to="/">Partenaires</Link>
+          <Link to="/">Contacts</Link>
+          <Link to="login">Login</Link>
+        </div>
+        <img className='LogoSearch' src="logo_search.png" alt="" />
       </div>
+
     </Wrapper>
   )
 }
@@ -16,20 +28,65 @@ const Wrapper = styled.nav`
   height: var(--nav-height);
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin: 20px 0 15% 0;
   box-shadow: 0 1px 0px 0px rgba(0, 0, 0, 0.1);
-  .logo {
-    display: flex;
+
+  .LogoSearch{
+    width: 30px;
+  }
+  
+  a{
+    margin-left: 40px;
+    color: #F6EBD8;
+    display: inline;
+  }
+
+  a::after{
+    content: "";
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 3px;
+    background: #F6EBD8;
+    transform: scale(0);
+    transition: transform 0.2s ease-in-out;
+  }
+
+  a:hover::after{
+    transform: scale(1);
+  }
+
+  .containerNavElmt{
     align-items: center;
+  }
+
+  .LogoNav {
     width: 100px;
   }
+  
   .nav-center {
     display: flex;
     width: 90vw;
-    align-items: center;
+    align-items: center;   
     justify-content: space-between;
   }
-  .toggle-btn {
+ 
+  background: var(--black);
+  
+  .nav-center {
+    width: 85%;
+  }
+
+  /* .logo {
+    display: flex;
+    align-items: center;
+    width: 100px;
+  } */
+
+
+  /* .toggle-btn {
     background: transparent;
     border-color: transparent;
     font-size: 1.75rem;
@@ -37,38 +94,36 @@ const Wrapper = styled.nav`
     cursor: pointer;
     display: flex;
     align-items: center;
-  }
-  background: var(--white);
-  .btn-container {
+  } */
+
+
+  /* .btn-container {
     position: relative;
-  }
-  .btn {
+  } */
+  /* .btn {
     display: flex;
     align-items: center;
     justify-content: center;
     gap: 0 0.5rem;
     position: relative;
     box-shadow: var(--shadow-2);
-  }
+  } */
 
-  .logo-text {
+  /* .logo-text {
     display: none;
     margin: 0;
-  }
-  @media (min-width: 992px) {
+  } */
+  /* @media (min-width: 992px) {
     position: sticky;
     top: 0;
 
-    .nav-center {
-      width: 90%;
-    }
     .logo {
       display: none;
     }
     .logo-text {
       display: block;
     }
-  }
+  } */
 `
 
 export default Navbar
