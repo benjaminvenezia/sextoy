ef52bb49a2065b8510c2de5ece234766e34094e9
Benjamin Venezia <benjamin.venezia@crea-inseec.com>
2022-05-14 13:57:45 +0200

    fix loading bad connexion

--- client/src/components/BadConnexionLoader.js
+++ client/src/components/BadConnexionLoader.js
@@ -1,19 +1,38 @@
 import styled from 'styled-components'
 
 const BadConnexionLoader = () => {
-  return <Wrapper>Chargement</Wrapper>
+  return (
+    <Wrapper>
+      <div className="animation-container">
+        <p>Chargement ...</p>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
   text-align: center;
   position: absolute;
+
   bottom: 0;
-  top: 40%;
+  top: 0;
   left: 0;
   right: 0;
   z-index: 1000000;
   background-color: black;
   color: white;
+
+  .animation-container {
+    width: 50%;
+    margin: 0 auto;
+    padding-top: 10%;
+    text-align: center;
+
+    p {
+      margin: 0 auto;
+      font-size: 1.3rem;
+    }
+  }
 `
 
 export default BadConnexionLoader
