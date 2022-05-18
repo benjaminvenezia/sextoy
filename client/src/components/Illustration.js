3869180397261056af91727be4281ed57c2cd61f
Benjamin Venezia <benjamin.venezia@crea-inseec.com>
2022-05-18 08:58:49 +0200

    better illustrations position

--- client/src/components/Illustration.js
+++ client/src/components/Illustration.js
@@ -16,9 +16,19 @@ const Illustration = () => {
   useEffect(() => {
     const illustration = pickRandomIllustration()
     setIllu(illustration)
-  }, [illu])
+  }, [])
 
-  return <StyledImage src={illu} alt="an suggestive illustration" />
+  let filenameSplitted = illu.split('.')
+  filenameSplitted = filenameSplitted[0].split('/')
+  const illustrationName = filenameSplitted[filenameSplitted.length - 1]
+
+  if (illustrationName == 'illustration-1') {
+    return <Illu1 src={illu} alt="an suggestive illustration" />
+  } else if (illustrationName == 'illustration-2') {
+    return <Illu2 src={illu} alt="an suggestive illustration" />
+  } else if (illustrationName == 'illustration-3') {
+    return <Illu3 src={illu} alt="an suggestive illustration" />
+  }
 }
 
 const StyledImage = styled.img`
@@ -26,12 +36,43 @@ const StyledImage = styled.img`
   height: 60%;
   overflow: hidden;
   position: absolute;
-  right: 20%;
   opacity: 0.5;
   z-index: -100;
 
   @media (max-width: 992px) {
     width: 50%;
+    bottom: 0;
+  }
+`
+//masturbation
+const Illu1 = styled(StyledImage)`
+  height: 80%;
+  bottom: 0;
+  left: 0;
+
+  @media (max-width: 992px) {
+    width: 70%;
+    bottom: 0;
+  }
+`
+//main qui tient le petit dildo
+const Illu2 = styled(StyledImage)`
+  height: 75%;
+  left: 0;
+
+  @media (max-width: 992px) {
+    width: 70%;
+    bottom: 0;
+  }
+`
+
+//main qui tient le grand dildo
+const Illu3 = styled(StyledImage)`
+  height: 80%;
+  right: 20px;
+
+  @media (max-width: 992px) {
+    width: 70%;
     bottom: 0;
   }
 `
